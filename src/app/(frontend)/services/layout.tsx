b4ea61acd2b0180cb4c9a957281c9bf1cd42b2e4
fatemameem <fatemameem97@gmail.com>
2025-09-14 01:51:15 -0400

    feat: restructure Events, Podcasts, Services, and Subscribe pages with layouts, components, and routes

--- src/app/(frontend)/services/layout.tsx
+++ src/app/(frontend)/services/layout.tsx
@@ -1,9 +1,10 @@
-// app/contact/layout.tsx
+// app/services/layout.tsx
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Services | TECH-NEST",
-  description: "Explore our range of services designed to enhance your cybersecurity and AI ethics initiatives.",
+  description:
+    "Explore our range of services designed to enhance your cybersecurity and AI ethics initiatives.",
 };
 
 export default function ServicesLayout({
